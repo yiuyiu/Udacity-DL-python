ff977a615e6c5caac5b19dc83bc4125e0c6cbea1
yuyi <charlie@yuyideMacBook-Pro.local>
2018-01-17 17:04:02 +0800

    initial commit

---
+++ entertainment_center.py
@@ -0,0 +1,8 @@
+import media
+titanic = media.Movie('TITANIC','https://img3.doubanio.com/view/photo/l/public/p457760035.webp','https://www.youtube.com/watch?v=CHekzSiZjrY') 
+farewell_to_my_concubine = media.Movie('Farewell to My Concubine','https://img3.doubanio.com/view/photo/l/public/p1910813120.webp','https://www.youtube.com/watch?v=cC-_SLiRnJE')
+the_last_emperor = media.Movie('The Last Emperor','https://img3.doubanio.com/view/photo/l/public/p452088641.webp','https://www.youtube.com/watch?v=A4cH6g1wD5g')
+movies = [titanic,farewell_to_my_concubine,the_last_emperor]
+
+import fresh_tomatoes 
+fresh_tomatoes.open_movies_page(movies)
---
+++ media.py
@@ -0,0 +1,5 @@
+class Movie():
+    def __init__(self,title,post_image,youtube_url):
+        self.title = title
+        self.poster_image_url = post_image
+        self.trailer_youtube_url = youtube_url
