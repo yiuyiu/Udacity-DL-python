f364712f4171bd7208beaeb2f8696ed7387c614f
yuyi <charlie@192.168.0.106>
2018-01-21 21:18:21 +0800

    添加注释，以及编码方式

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,8 +1,14 @@
-import media
-titanic = media.Movie('TITANIC','https://img3.doubanio.com/view/photo/l/public/p457760035.webp','https://www.youtube.com/watch?v=CHekzSiZjrY') 
-farewell_to_my_concubine = media.Movie('Farewell to My Concubine','https://img3.doubanio.com/view/photo/l/public/p1910813120.webp','https://www.youtube.com/watch?v=cC-_SLiRnJE')
-the_last_emperor = media.Movie('The Last Emperor','https://img3.doubanio.com/view/photo/l/public/p452088641.webp','https://www.youtube.com/watch?v=A4cH6g1wD5g')
+# coding=utf-8
+import media #引入电影类
+titanic = media.Movie('TITANIC','https://img3.doubanio.com/view/photo/l/public/p457760035.webp',\
+                      'https://www.youtube.com/watch?v=CHekzSiZjrY') 
+farewell_to_my_concubine = media.Movie('Farewell to My Concubine',\
+                      'https://img3.doubanio.com/view/photo/l/public/p1910813120.webp',\
+                      'https://www.youtube.com/watch?v=cC-_SLiRnJE')
+the_last_emperor = media.Movie('The Last Emperor',\
+                      'https://img3.doubanio.com/view/photo/l/public/p452088641.webp',\
+                      'https://www.youtube.com/watch?v=A4cH6g1wD5g')
 movies = [titanic,farewell_to_my_concubine,the_last_emperor]
 
-import fresh_tomatoes 
+import fresh_tomatoes #引入电影页模板
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,5 +1,14 @@
+# coding=utf-8
 class Movie():
+    """
+    定义电影类
+    Attributes:
+        title: 电影标题.
+        post_image: 电影海报图片地址.
+        youtube_url: 电影youtube播放链接
+    """
     def __init__(self,title,post_image,youtube_url):
+        '''实例化电影类，接受电影标题，海报图片地址，youtube播放链接参数'''
         self.title = title
         self.poster_image_url = post_image
         self.trailer_youtube_url = youtube_url
